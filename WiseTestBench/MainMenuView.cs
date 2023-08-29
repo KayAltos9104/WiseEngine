a4ef7f0e318069641a7d789666fef3bbf7ac1a34
Kay Altos <kayaltosnexus@gmail.com>
2023-08-29 21:42:15 +0300

    - Added Test Project

---
+++ WiseTestBench/MainMenuView.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WiseEngine.MonogamePart;
+using WiseEngine;
+using WiseEngine.MVP;
+using WiseEngine.UI;
+using Microsoft.Xna.Framework;
+
+namespace WiseTestBench
+{
+    public class MainMenuView : View
+    {
+        public int CursorPos { get; set; } = 0;
+
+        public override void Initialize()
+        {
+            MessageBox MbxTest = new MessageBox(new Vector2(
+                    Globals.Resolution.Width / 2, Globals.Resolution.Height / 2 - 100),
+                    LoadableObjects.GetFont("MainFont"),
+                    "Тестовое окно\nНажмите Q, чтобы поменять текст"
+                    );
+            MbxTest.IsCentered = true;
+
+
+            Button BtnTest = new Button(new Vector2(
+                    Globals.Resolution.Width / 2, Globals.Resolution.Height / 2 + 100),
+                    LoadableObjects.GetFont("MainFont"), "Перейти к сцене 2");
+            Button BtnTest2 = new Button(new Vector2(
+                    Globals.Resolution.Width / 2, Globals.Resolution.Height / 2 + 150),
+                    LoadableObjects.GetFont("MainFont"), "Нет, меня!");
+            Button BtnTest3 = new Button(new Vector2(
+                    Globals.Resolution.Width / 2, Globals.Resolution.Height / 2 + 250),
+                    LoadableObjects.GetFont("MainFont"), "Выход");
+
+            BtnTest.Click += BtnTest1_Click;
+            BtnTest2.Click += BtnTest2_Click;
+            BtnTest3.Click += BtnTest3_Click;
+
+            _interfaceManager.AddElement(MbxTest);
+            _interfaceManager.AddElement(BtnTest);
+            _interfaceManager.AddElement(BtnTest2);
+            _interfaceManager.AddElement(BtnTest3);
+
+            //((IKeyboardCursor)_interfaceManager).UpdateActivationOnElement();
+        }
+
+        public override void Update()
+        {
+
+            if (InputsManager.IsSinglePressed(Keys.W))
+                ((IKeyboardCursor)_interfaceManager).MoveCursor(DiscreteDirection.Up);
+            if (InputsManager.IsSinglePressed(Keys.S))
+                ((IKeyboardCursor)_interfaceManager).MoveCursor(DiscreteDirection.Down);
+            if (InputsManager.IsSinglePressed(Keys.Space))
+                (((IKeyboardCursor)_interfaceManager).GetCurrentElement() as Button).PerformClick();
+
+            base.Update();
+        }
+
+        public override void LoadModelData(ModelViewData currentModelData)
+        {
+            throw new NotImplementedException();
+        }
+        private void BtnTest1_Click(object sender, ClickEventArgs e)
+        {
+            //MessageBox MbxTest1 = new MessageBox(new Vector2(
+            //        250, 30),
+            //        LoadableObjects.GetFont("MainFont"),
+            //        "Нажата кнопка 1"
+            //        );
+            //MbxTest1.IsCentered = true;
+
+            //_interfaceElements.Add("MbxTest1", MbxTest1);
+            OnSceneFinished(new SceneFinishedEventArgs() { NewSceneName = "Test2" });
+        }
+        private void BtnTest2_Click(object sender, ClickEventArgs e)
+        {
+            //MessageBox MbxTest2 = new MessageBox(new Vector2(
+            //        250, 60),
+            //        LoadableObjects.GetFont("MainFont"),
+            //        "Нажата кнопка 2"
+            //        );
+            //MbxTest2.IsCentered = true;
+
+            //_interfaceElements.Add("MbxTest2", MbxTest2);
+        }
+        private void BtnTest3_Click(object sender, ClickEventArgs e)
+        {
+            //MessageBox MbxTest3 = new MessageBox(new Vector2(
+            //        250, 90),
+            //        LoadableObjects.GetFont("MainFont"),
+            //        "Заглушка на выход из программы"
+            //        );
+            //MbxTest3.IsCentered = true;
+
+            //_interfaceElements.Add("MbxTest3", MbxTest3);
+        }
+    }
+}
---
+++ WiseTestBench/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System;
+using WiseEngine.MonogamePart;
+using WiseEngine.MVP;
+using WiseTestBench;
+using WiseEngine;
+
+string currentDirectory = Directory.GetCurrentDirectory();
+string projectRoot = Path.GetFullPath(Path.Combine(currentDirectory, "..\\..\\..\\"));
+Globals.ResourcesPath = projectRoot+"\\Resources";
+var game = new GameProcessor(
+    new List<(string, string)>(),
+    //{
+    //            ("base_car","Base_car")
+    //},
+    new List<(string, string)>()
+    {
+                ("MainFont", "DescriptionFont")
+    }
+    );
+
+var v1 = new MainMenuView();
+var scene1 = new Scene(v1, null, new Presenter(game, v1, null));
+game.Scenes.Add("Test1", scene1);
+
+//var v2 = new SettingsMenuView();
+//var scene2 = new Scene(v2, null, new Presenter(game, v2, null));
+//game.Scenes.Add("Test2", scene2);
+
+game.SetCurrentScene("Test1");
+game.Run();
